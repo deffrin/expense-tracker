9be858253b9cf380378dd33d53770fd03ae864e8
Deffrin Joseph <deffrin@gmail.com>
2026-07-02 13:35:18 +0530

    create categories and expenses table

---
+++ database/migrations/2026_07_02_073656_create_expenses_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('amount', 10, 2);
+            $table->text('description');
+            $table->timestamp('added_date');
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('user_id');
+
+            $table->timestamps();
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->restrictOnDelete();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
